877077f673c101b89062ed53b63d0dae2e645339
Cao Manh Dung <dungcaond@gmail.com>
2016-03-08 08:11:36 +0700

    add version 3 for task 1

---
+++ task1/v3/home.php
@@ -0,0 +1,3 @@
+<h1>Welcome to AdventureWorks</h1>
+<hr/>
+<?php @include('include/menu.php') ?>
---
+++ task1/v3/include/database.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database{
+	public function getCustomers(){
+		$conn = mysqli_connect('localhost', 'root', '123456', 'adventureworks');
+		// Check connection
+		if (!$conn) {
+		    die("Connection failed: " . mysqli_connect_error());
+		}
+
+		$result = mysqli_query($conn, 'SELECT * FROM customer');
+
+		mysqli_close($conn);
+
+		return $result;
+	}
+	public function getEmployees(){
+		$conn = mysqli_connect('localhost', 'root', '123456', 'adventureworks');
+		// Check connection
+		if (!$conn) {
+		    die("Connection failed: " . mysqli_connect_error());
+		}
+
+		$result = mysqli_query($conn, 'SELECT * FROM employee');
+
+		mysqli_close($conn);
+
+		return $result;
+	}
+	
+}
+
